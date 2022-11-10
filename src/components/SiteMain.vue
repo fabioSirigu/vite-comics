615e8ccd3da03a7254d84177e3d108f75b67a1e3
naz1ne <fabio.sirigu.1995@gmail.com>
2022-11-10 15:50:26 +0100

    add section icon

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,12 +1,46 @@
 <script>
 export default {
       name: 'SiteMain',
+      data() {
+            return {
+                  icons: [
+                        {
+                              image: '../assets/img/buy-comics-digital-comics.png',
+                              text: 'Digital comics'
+                        },
+                        {
+                              image: '',
+                              text: 'Digital comics'
+                        },
+                        {
+                              image: '',
+                              text: 'Digital comics'
+                        },
+                        {
+                              image: '',
+                              text: 'Digital comics'
+                        },
+                        {
+                              image: '',
+                              text: 'Digital comics'
+                        }
+                  ]
+            }
+      }
 }
 </script>
 <template>
       <main id="site_main">
             <div class="jumbotron">
                   <h1> --> Content Goes Here </h1>
+            </div>
+            <div class="my-container">
+                  <div class="row">
+                        <div class="col" v-for="icon in icons">
+                              <img :src="icon.image" alt="">
+                              <h3>{{ icon.text }}</h3>
+                        </div>
+                  </div>
             </div>
             <!-- /.jumbotron -->
       </main>
