4117b415171498bc886cc647e031a6296b3e1749
naz1ne <fabio.sirigu.1995@gmail.com>
2022-11-10 15:09:30 +0100

    initial commit add header

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import SiteHeader from './components/SiteHeader.vue'
+export default {
+  name: 'App',
+  components: {
+    SiteHeader
+  }
+}
+</script>
+
+<template>
+  <SiteHeader />
+  <!-- /#site_header -->
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,69 @@
+<script>
+export default {
+      name: 'SiteHeader',
+      data() {
+            return {
+                  menu: [
+                        {
+                              text: 'characters',
+                              href: '#'
+                        },
+                        {
+                              text: 'comics',
+                              href: '#'
+                        },
+                        {
+                              text: 'movies',
+                              href: '#'
+                        },
+                        {
+                              text: 'tv',
+                              href: '#'
+                        },
+                        {
+                              text: 'games',
+                              href: '#'
+                        },
+                        {
+                              text: 'collectibles',
+                              href: '#'
+                        },
+                        {
+                              text: 'videos',
+                              href: '#'
+                        },
+                        {
+                              text: 'fans',
+                              href: '#'
+                        },
+                        {
+                              text: 'news',
+                              href: '#'
+                        },
+                        {
+                              text: 'shop',
+                              href: '#'
+                        }
+                  ]
+            }
+      }
+}
+</script>
+<template>
+      <header id="site_header">
+            <div class="container">
+                  <div class="logo">
+                        <img src="../assets/img/dc-logo.png" alt="">
+                  </div>
+                  <nav class="navbar">
+                        <a :href="item.href" v-for="item in menu" :class="item.text === 'comics' ? 'active' : ''">
+                              {{ item.text }}
+                        </a>
+                  </nav>
+                  <!-- /.navbar -->
+            </div>
+      </header>
+</template>
+<style>
+
+</style>
