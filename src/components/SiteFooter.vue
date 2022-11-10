1ea40f58dd0c9b8c145e7acdc8b38e0b9baba19d
naz1ne <fabio.sirigu.1995@gmail.com>
2022-11-10 17:47:51 +0100

    add bottom footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,60 +1,169 @@
 <script>
 export default {
-      name: 'SiteFooter'
+      name: 'SiteFooter',
+      data() {
+            return {
+                  DcComics: [
+                        {
+                              text: 'Characters'
+                        },
+                        {
+                              text: 'Comics'
+                        },
+                        {
+                              text: 'Movies'
+                        },
+                        {
+                              text: 'TV'
+                        },
+                        {
+                              text: 'Games'
+                        },
+                        {
+                              text: 'Videos'
+                        },
+                        {
+                              text: 'News'
+                        }
+                  ],
+                  Shop: [
+                        {
+                              text: 'Shop DC'
+                        },
+                        {
+                              text: 'Shop DC Collectibles'
+                        }
+                  ],
+                  DC: [
+                        {
+                              text: 'Terms Of Use'
+                        },
+                        {
+                              text: 'Privacy policy (New)'
+                        },
+                        {
+                              text: 'Ad Choices'
+                        },
+                        {
+                              text: 'Advertising'
+                        },
+                        {
+                              text: 'Jobs'
+                        },
+                        {
+                              text: 'Subscriptions'
+                        },
+                        {
+                              text: 'Talent Workshops'
+                        },
+                        {
+                              text: 'CPSC Certificates'
+                        },
+                        {
+                              text: 'Ratings'
+                        },
+                        {
+                              text: 'Shop Help'
+                        },
+                        {
+                              text: 'Contact Us'
+                        },
+                  ],
+                  Sites: [
+                        {
+                              text: 'DC'
+                        },
+                        {
+                              text: 'MAD Magazine'
+                        },
+                        {
+                              text: 'DC Kids'
+                        },
+                        {
+                              text: 'DC Universe'
+                        },
+                        {
+                              text: 'DC Power Visa'
+                        }
+                  ],
+                  Icons: [
+                        {
+                              url: 'footer-facebook.png',
+                              name: 'facebook'
+                        },
+                        {
+                              url: 'footer-periscope.png',
+                              name: 'facebook'
+                        },
+                        {
+                              url: 'footer-pinterest.png',
+                              name: 'facebook'
+                        },
+                        {
+                              url: 'footer-twitter.png',
+                              name: 'facebook'
+                        },
+                        {
+                              url: 'footer-youtube.png',
+                              name: 'facebook'
+                        },
+                  ]
+            }
+      }
 }
 </script>
 <template>
       <footer id="site_footer">
-            <div class="my_container">
-                  <div class="row">
-                        <div class="col">
-                              <div class="row">
-                                    <div class="col">
-                                          <h3>dc comics</h3>
+            <div class="contact_us">
+                  <div class="my_container">
+                        <div class="row">
+                              <div class="col">
+                                    <div class="row">
+                                          <div class="col">
+                                                <h3>dc comics</h3>
+                                                <a href="#" v-for="item in DcComics">{{ item.text }}</a>
 
-                                          <a href="#">Characters</a>
-                                          <a href="#">Comics</a>
-                                          <a href="#">Movies</a>
-                                          <a href="#">TV</a>
-                                          <a href="#">Games</a>
-                                          <a href="#">Videos</a>
-                                          <a href="#">News</a>
-                                          <h3>shop</h3>
+                                                <h3>shop</h3>
+                                                <a href="#" v-for="item in Shop">{{ item.text }}</a>
 
-                                          <a href="#">Shop DC</a>
-                                          <a href="#">Shop DC Collectibles</a>
 
-                                    </div>
-                                    <div class="col">
-                                          <h3>DC</h3>
-                                          <a href="#">Terms Of Use</a>
-                                          <a href="#">Privacy poacy (New)</a>
-                                          <a href="#">Ad Choices</a>
-                                          <a href="#">Advertising</a>
-                                          <a href="#">Jobs</a>
-                                          <a href="#">Subscriptions</a>
-                                          <a href="#">Talent Workshops</a>
-                                          <a href="#">CPSC Certificates</a>
-                                          <a href="#">Ratings</a>
-                                          <a href="#">Shop Help</a>
-                                          <a href="#">Contact Us</a>
-                                    </div>
-                                    <div class="col">
-                                          <h3>Sites</h3>
+                                          </div>
+                                          <div class="col">
+                                                <h3>DC</h3>
+                                                <a href="#" v-for="item in DC">{{ item.text }}</a>
 
-                                          <a href="#">DC</a>
-                                          <a href="#">MAD Magazine</a>
-                                          <a href="#">DC Kids</a>
-                                          <a href="#">DC Universe</a>
-                                          <a href="#">DC Power Visa</a>
+                                          </div>
+                                          <div class="col">
+                                                <h3>Sites</h3>
 
+                                                <a href="#" v-for="item in Sites">{{ item.text }}</a>
+
+
+                                          </div>
                                     </div>
                               </div>
-                        </div>
-                        <div class="col">
-                              <img src="../assets/img/dc-logo-bg.png" alt="">
+                              <div class="col-7">
+                                    <!-- <img src="../assets/img/dc-logo-bg.png" alt=""> -->
+                              </div>
                         </div>
                   </div>
+
+            </div>
+            <div class="call_to_action">
+                  <div class="my_container">
+                        <button>
+                              sign-up now!
+                        </button>
+                        <nav class="navbar">
+                              <h3>follow us</h3>
+                              <div>
+                                    <a v-for="icon in Icons" href="#">
+                                          <img :src="'../src/assets/img/' + icon.url" alt="">
+                                    </a>
+                              </div>
+                        </nav>
+                  </div>
+
             </div>
       </footer>
       <!-- /#site_footer -->
