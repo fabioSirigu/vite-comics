739a9dacb5428b17c55306eef91c8020d32530e7
naz1ne <fabio.sirigu.1995@gmail.com>
2022-11-10 18:00:37 +0100

    add bonus

---
+++ src/components/IconSection.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+      name: 'IconSection',
+      data() {
+            return {
+                  icons: [
+                        {
+                              image: 'buy-comics-digital-comics.png',
+                              text: 'Digital Comics'
+                        },
+                        {
+                              image: 'buy-comics-merchandise.png',
+                              text: 'DC Merchandise'
+                        },
+                        {
+                              image: 'buy-comics-subscriptions.png',
+                              text: 'Subscriction'
+                        },
+                        {
+                              image: 'buy-comics-shop-locator.png',
+                              text: 'Comics shop locator'
+                        },
+                        {
+                              image: 'buy-dc-power-visa.svg',
+                              text: 'DC power visa'
+                        }
+                  ]
+            }
+      }
+}
+</script>
+<template>
+      <div class="my-container">
+            <div class="row">
+                  <div class="col" v-for="icon in icons">
+                        <img :src="'./src/assets/img/' + icon.image" alt="">
+                        <h3>{{ icon.text }}</h3>
+                  </div>
+            </div>
+      </div>
+</template>
+<style scoped>
+
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,32 +1,11 @@
 <script>
+import IconSection from './IconSection.vue'
 export default {
       name: 'SiteMain',
-      data() {
-            return {
-                  icons: [
-                        {
-                              image: 'buy-comics-digital-comics.png',
-                              text: 'Digital Comics'
-                        },
-                        {
-                              image: 'buy-comics-merchandise.png',
-                              text: 'DC Merchandise'
-                        },
-                        {
-                              image: 'buy-comics-subscriptions.png',
-                              text: 'Subscriction'
-                        },
-                        {
-                              image: 'buy-comics-shop-locator.png',
-                              text: 'Comics shop locator'
-                        },
-                        {
-                              image: 'buy-dc-power-visa.svg',
-                              text: 'DC power visa'
-                        }
-                  ]
-            }
+      components: {
+            IconSection
       }
+
 }
 </script>
 <template>
@@ -34,14 +13,7 @@ export default {
             <div class="jumbotron">
                   <h1> --> Content Goes Here </h1>
             </div>
-            <div class="my-container">
-                  <div class="row">
-                        <div class="col" v-for="icon in icons">
-                              <img :src="'./src/assets/img/' + icon.image" alt="">
-                              <h3>{{ icon.text }}</h3>
-                        </div>
-                  </div>
-            </div>
+            <IconSection />
             <!-- /.jumbotron -->
       </main>
 </template>
