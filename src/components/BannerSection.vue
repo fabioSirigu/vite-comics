ad4717eb038c73c1e7c9fd3495bced2fd9baaeeb
naz1ne <fabio.sirigu.1995@gmail.com>
2022-11-11 15:54:15 +0100

    final commit

---
+++ src/components/BannerSection.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+      name: 'BannerSection'
+}
+</script>
+<template>
+      <section class="banner">
+            <div class="container">
+                  <h3>Current series</h3>
+
+            </div>
+
+      </section>
+</template>
+<style>
+
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,12 +1,14 @@
 <script>
 import IconSection from './IconSection.vue'
 import ComicsCard from './ComicsCard.vue'
+import BannerSection from './BannerSection.vue'
 import Comics from './data/Comics.js'
 export default {
       name: 'SiteMain',
       components: {
             IconSection,
-            ComicsCard
+            ComicsCard,
+            BannerSection
       },
       data() {
             return {
@@ -18,12 +20,16 @@ export default {
 </script>
 <template>
       <main id="site_main">
+            <BannerSection />
+
             <div class="jumbotron">
                   <div class="container">
                         <div class="row">
                               <ComicsCard v-for="comic in comics" :url="comic.thumb" :title="comic.series" />
-
                         </div>
+                  </div>
+                  <div class="load_more">
+                        <h3>Load More</h3>
                   </div>
             </div>
             <IconSection />
